5ac641a4babe57000aa1e879f5e6433e9fbd07d4
itsriver15 <riveranderson.5213@gmail.com>
2026-07-02 20:23:33 -0600

    Removed staging

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -6,7 +6,6 @@
 
 // That means that it looks like there are a lot more shapes then there actually are in each stage, but my for loop demonstrates that the numbers are correct :]
 int main(int argc, char* argv[]) {
-    int stage = 0;
     Renderer renderer;
     SDL_Init(SDL_INIT_VIDEO);
     renderer.CreateWindow(1280, 1024);
@@ -24,30 +23,23 @@ int main(int argc, char* argv[]) {
                quit = true;
             }
 
-            if (e.type == SDL_EVENT_KEY_DOWN &&
-                e.key.key == SDLK_RETURN)
-            {
-                stage++;
-                
-            }
         }
 
 
         renderer.SetColor(0, 0, 0, 255);
         renderer.ClearScreen();
         int i = 0;
-        if (stage == 0)
-        {
             // Draw rectangles - I have it only do one since the number of triangles was unspecified.
+            for (i; i < 3; i++) {
                 renderer.SetColor(rand() % 256, rand() % 256, rand() % 256, 255);
 
                 int width = rand() % 100 + 20;
                 int height = rand() % 100 + 20;
 
                 renderer.DrawRectangle(rand() % (1280 - width), rand() % (1024 - height), width, height);
-        }
-        else if (stage == 1)
-        {
+            }
+
+      
             i = 0;
             // Draw all lines
             for (i; i < 10; i++)
@@ -56,9 +48,7 @@ int main(int argc, char* argv[]) {
 
                 renderer.DrawLine(rand() % 1280, rand() % 1024, rand() % 1280, rand() % 1024);
             }
-        }
-        else if (stage == 2)
-        {
+        
             i = 0;
             // Draw all points
             for (i; i < 20; i++)
@@ -67,10 +57,7 @@ int main(int argc, char* argv[]) {
 
                 renderer.DrawPoint(rand() % 1280, rand() % 1024);
             }
-        }
-        else {
-            break;
-        }
+       
 
         renderer.PresentScreen();
     }
